b9877c898bffa4f365f63fadb4a8e1c19d8aca45
cardon <michel-cardon@laposte.net>
2019-07-30 14:27:38 +0200

    selectedpizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,16 @@ export class Pizza {
   id : number;
   name: string;
   price:number;
+  image:string;
 }
+
+const PIZZAS : Pizza[] = [
+  { id: 1, name: 'Reine', price: 12,image : "p1.jpg" },
+  { id: 2, name: '4 fromages', price: 13, image : "p2.jpg"},
+  { id: 3, name: 'Orientale', price: 11, image : "p3.jpg"},
+  { id: 4, name: 'Cannibale', price: 9, image : "p4.jpg" }
+];
+
 
 @Component({
   selector: 'app-root',
@@ -16,9 +25,12 @@ export class AppComponent {
  
   title = 'pizzaparty';
   name='4fromages';
-  pizza : Pizza = {
-    id : 1,
-    name : '4 fromages',
-    price : 10
+  pizzas = PIZZAS;
+  selectedPizza : Pizza;
+  onSelect(pizza:Pizza){
+    console.log (pizza);
+    //on modif la prop pizz  de l'instance 
+    //de AppComponent
+    this.selectedPizza=pizza;
   }
 }
